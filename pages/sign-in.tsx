cdbb5dd90c1c7280f048c4a190671f90935f8b75
Ivan Boiko <ivan.boiko.code@gmail.com>
2023-09-19 14:52:54 +0300

    fix(route): sign-in route

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { Box, Button } from '@mui/material';
 import { useTranslation } from 'next-i18next';
 import type { NextPage, GetStaticProps } from 'next';
-import { useSession, signIn, signOut } from 'next-auth/react';
+// import { useSession, signIn, signOut } from 'next-auth/react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { Typography } from '@app/old/skeleton';
@@ -10,10 +10,10 @@ import {} from '@app/old/auth/components/SignIn';
 
 const ErrorPage: NextPage = () => {
   const { t } = useTranslation('common');
-  const { data } = useSession();
+  // const { data } = useSession();
   const { back } = useRouter();
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <Box display="flex" flexDirection="column">
@@ -26,12 +26,12 @@ const ErrorPage: NextPage = () => {
         </Button>
       </Box>
       <Box mt={2} display="flex" mx="auto" gap={2}>
-        <Button variant="contained" onClick={() => signIn()}>
-          Sign In
-        </Button>
-        <Button variant="contained" onClick={() => signOut()}>
-          Sign Out
-        </Button>
+        {/*<Button variant="contained" onClick={() => signIn()}>*/}
+        {/*  Sign In*/}
+        {/*</Button>*/}
+        {/*<Button variant="contained" onClick={() => signOut()}>*/}
+        {/*  Sign Out*/}
+        {/*</Button>*/}
       </Box>
     </Box>
   );
