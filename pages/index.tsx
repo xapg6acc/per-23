7423c47ef3d231e53097ea2278384802b75f5e35
Ivan Boiko <ivan.boiko.code@gmail.com>
2024-02-03 17:15:45 +0200

    main page meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,16 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { theme } from '@app/home-page/configs/theme';
 import { MainPage } from '@app/home-page/components/main/MainPage';
+import image from '@app/home-page/images/ua-flag.png';
 
 const Main: NextPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Head>
         <title>Main Page</title>
+        <meta property="og:title" content="Ivan Boiko draft" />
+        <meta property="og:description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit." />
+        <meta property="og:image" content={image.src} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
