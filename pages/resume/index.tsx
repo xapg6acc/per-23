15a94c8f7de2ff1f134a272c43c1a5434e45289b
Ivan Boiko <ivan.boiko.code@gmail.com>
2024-02-03 17:26:39 +0200

    cv meta

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -3,12 +3,16 @@ import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { CvPage as Cv } from '@app/cv-page/components/cv';
+import photoIvanBoiko from '@app/cv-page/images/photoIvanBoiko.jpg';
 
 const CvPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>CV Page</title>
+        <meta property="og:title" content="Ivan Boiko CV" />
+        <meta property="og:description" content="Frontend engineer (developer)" />
+        <meta property="og:image" content={photoIvanBoiko.src} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Cv />
