3ea04c10c30cd229e2e6f73941e3e11a0127239d
Ivan Boiko <ivan.boiko.code@gmail.com>
2024-02-09 18:15:22 +0200

    draft meta

--- pages/draft/index.tsx
+++ pages/draft/index.tsx
@@ -3,12 +3,16 @@ import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { PerCPage } from '@app/per-c/components/page';
+import image from '@app/home-page/images/Rioters-New-York-Tribune-offices-Republican-Draft-1863.webp';
 
 const PerPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>Drafts Page</title>
+        <meta property="og:title" content="lorem ipsum" />
+        <meta property="og:description" content="draft components (dolor sin amet)" />
+        <meta property="og:image" content={image.src} />
       </Head>
       <main>
         <PerCPage />
