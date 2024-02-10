28d4ba53d370626527cc59d430445a32fe17aaca
Ivan Boiko <ivan.boiko.code@gmail.com>
2024-02-10 12:00:28 +0200

    draft

--- src/per-c/components/page.tsx
+++ src/per-c/components/page.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
 import { Ticker } from './ticker';
 import { CAccordion } from '@app/per-c/components/accordion';
@@ -10,31 +10,57 @@ export const PerCPage = () => {
   const { push } = useRouter();
 
   return (
-    <Box display="flex" flexDirection="column" minHeight="100vh">
+    <Box display="flex" flexDirection="column" minHeight="100vh" gap={5} bgcolor="lightyellow">
       <Box p={5}>
         <Button fullWidth variant="outlined" onClick={() => push('/')}>
           back
         </Button>
+      </Box>
+      <Box height={600} position="relative">
+        <Box
+          display="flex"
+          position="sticky"
+          top={0}
+          height={200}
+          sx={{ backgroundColor: 'rgba(29, 29, 31, 0.2)', backdropFilter: 'saturate(180%) blur(10px)' }}
+        ></Box>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate enim incidunt magnam placeat quasi,
+        quisquam totam. Ab aliquam animi atque dolorem error fugiat impedit molestias optio, ullam! Autem, quia,
+        repudiandae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate enim incidunt magnam placeat
+        quasi, quisquam totam. Ab aliquam animi atque dolorem error fugiat impedit molestias optio, ullam! Autem, quia,
+        repudiandae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate enim incidunt magnam placeat
+        quasi, quisquam totam. Ab aliquam animi atque dolorem error fugiat impedit molestias optio, ullam! Autem, quia,
+        repudiandae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate enim incidunt magnam placeat
+        quasi, quisquam totam. Ab aliquam animi atque dolorem error fugiat impedit molestias optio, ullam! Autem, quia,
+        repudiandae.
       </Box>
       <Ticker direction="right">
         {tickerList.map((item, index) => (
           <Box
             key={item}
             width={250}
-            bgcolor={index === 0 ? 'gray' : 'black'}
             borderRadius={5}
             height={200}
             display="flex"
             justifyContent="center"
             alignItems="center"
-            color="common.white"
+            color={index === 0 ? 'rgba(29, 29, 31, 0.2)' : 'common.white'}
             fontSize={40}
+            overflow="hidden"
+            sx={{
+              backgroundColor: index === 0 ? 'rgba(255,255,255,0.72)' : 'rgba(29, 29, 31, 0.2)',
+              backdropFilter: 'saturate(180%) blur(10px)',
+            }}
           >
             {item}
           </Box>
         ))}
       </Ticker>
-      <Box mt="auto" bgcolor="lightgray" borderRadius={1}>
+      <Box
+        bgcolor="rgba(29, 29, 31, 0.72)"
+        borderRadius={1}
+        sx={{ backgroundColor: 'rgba(29, 29, 31, 0.2)', backdropFilter: 'saturate(180%) blur(10px)' }}
+      >
         <CAccordion />
       </Box>
     </Box>
